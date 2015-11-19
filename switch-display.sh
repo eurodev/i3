ddb3a546fdc88d767b382f1be1e6482e00333d66
Javier Carranza <javier.carranza@crononauta.com>
2015-11-19 19:14:36 +0100

    Provides the ability to detect if the second monitor is connected or not. Cleans and securizes the script.

--- switch-display.sh
+++ switch-display.sh
@@ -2,7 +2,47 @@
 #
 # Set VGA monitor to the left of the laptop and primary.
 
-xrandr --output LVDS1 --auto
-xrandr --output VGA1 --off
-xrandr --output VGA1 --left-of LVDS1 --auto
-xrandr --output VGA1 --primary
+# Private variables
+XRANDR=$(which xrandr)
+GREP=$(which grep)
+WC=$(which wc)
+
+function check() {
+
+    if [ -z "${XRANDR}" ]
+    then
+        echo -e "xrandr is not installed or in your path. Please, consider to install it in order to use this script"
+        echo
+        exit 2
+    fi
+
+    if [ -z "${GREP}" ]
+    then
+        echo -e "grep is not installed or in your path. Please, consider to install it in order to use this script"
+        echo
+        exit 2
+    fi
+
+    if [ -z "${WC}" ]
+    then
+        echo -e "wc is not installed or in your path. Please, consider to install it in order to use this script"
+        echo
+        exit 2
+    fi
+}
+
+function switch() {
+
+    NUM_DISPLAYS=$(${XRANDR} | ${GREP} " connected " | ${WC} -l)
+
+    if [ $NUM_DISPLAYS -eq 2 ]
+    then
+        ${XRANDR} --output LVDS1 --auto
+        ${XRANDR} --output VGA1 --off
+        ${XRANDR} --output VGA1 --left-of LVDS1 --auto
+        ${XRANDR} --output VGA1 --primary
+    fi
+}
+
+check
+switch
